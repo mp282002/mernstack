a4a88a06103cda4bb7a8f291bcd5c6a52b2ca2f7
patil mayuri manohar <mp7716113@gmail.com>
2024-06-06 11:06:38 +0530

    Add files via upload

---
+++ mernstack/frontend/src/components/About.jsx
@@ -0,0 +1,38 @@
+
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const About = () => {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="banner">
+            <div className="top">
+              <h1 className="heading">ABOUT US</h1>
+              <p>The only thing we're serious about is food.</p>
+            </div>
+            <p className="mid">
+            We are delighted that you have chosen our hotel. 
+            On behalf of the entire team of the (code with zeshoo), 
+            I extend a very warm welcome to you and trust that your stay with us will be pleasant and comfortable. 
+            Dear (name of guest), The management and staff are pleased to be your hosts.
+            </p>
+            <Link to={"/"}>
+              Explore Menu{" "}
+              <span>
+                <HiOutlineArrowRight />
+              </span>
+            </Link>
+          </div>
+          <div className="banner">
+            <img src="about.png" alt="about" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
